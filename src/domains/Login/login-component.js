abf825e27cf399f2c844dbe2a10ffee8e96000ce
buidohuy99 <sunnyhill93@gmail.com>
2020-12-15 22:36:56 +0700

    remove FB login

--- src/domains/Login/login-component.js
+++ src/domains/Login/login-component.js
@@ -1,6 +1,4 @@
-/*global FB*/
-
-import React, { useEffect, useState, useCallback } from 'react';
+import React, {useState} from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -14,8 +12,6 @@ import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Axios from 'axios';
-
-import ReactDOM from 'react-dom';
 
 import API from "../../services/api.js";
 import { Redirect } from 'react-router-dom';
@@ -82,91 +78,6 @@ export default function Login({onLogin}) {
     }
   }
 
-  // This is called with the results from from FB.getLoginStatus().
-  const statusChangeCallback = useCallback(async (response) => {
-    console.log('statusChangeCallback');
-    console.log(response);
-    // The response object is returned with a status field that lets the
-    // app know the current login status of the person.
-    // Full docs on the response object can be found in the documentation
-    // for FB.getLoginStatus().
-    if (response.status === 'connected') {
-      const fbloginbutton = document.getElementById("thisisafacebookbutton");  
-
-      /*********** Send access token to backend here ***********/
-
-      setAuthTokens(response.authResponse.accessToken);
-      if(fbloginbutton) fbloginbutton.remove();
-      // Logged into your app and Facebook.
-      setIsLoggedIn(true);
-    } else {
-      setLoginMessage("User is not authorized");
-      setIsLoggedIn(false);
-    }
-  }, [setAuthTokens]);
-
-  // This function is called when someone finishes with the Login
-  // Button.  See the onlogin handler attached to it in the sample
-  // code below.
-  const checkLoginState = useCallback(async () => {
-    FB.getLoginStatus((response) => { 
-      //Turn on loading dropback before calling this
-      statusChangeCallback(response);
-    });
-  }, [statusChangeCallback]);
-
-  // Init FB login for this component
-  useEffect(() => {
-
-    window.fbAsyncInit = () => {
-      FB.init({
-        appId      : process.env.REACT_APP_FACEBOOK_APP_ID,
-        cookie     : true,  // enable cookies to allow the server to access
-                          // the session
-        xfbml      : true,  // parse social plugins on this page
-        version    : 'v2.6' // use version 2.6
-      });
-  
-      // Now that we've initialized the JavaScript SDK, we call
-      // FB.getLoginStatus().  This function gets the state of the
-      // person visiting this page and can return one of three states to
-      // the callback you provide.  They can be:
-      //
-      // 1. Logged into your app ('connected')
-      // 2. Logged into Facebook, but not your app ('not_authorized')
-      // 3. Not logged into Facebook and can't tell if they are logged into
-      //    your app or not.
-      //
-      // These three cases are handled in the callback function.      
-
-      FB.Event.subscribe('auth.statusChange', async (response) => {
-        if (response.authResponse) {
-          checkLoginState();
-        } else {
-          console.log('---->User is not fully authorized.');
-        }
-      });
-    };
-
-    // Load the SDK asynchronously
-    ((d, s, id) => {
-      const existingScript = d.getElementById(id);
-      if (existingScript) {
-        ReactDOM.render(<div id="FBLogin" className="fb-login-button" data-size="large" data-button-type="login_with" data-layout="default" data-auto-logout-link="false" data-use-continue-as="false" data-width=""></div>, d.getElementById("facebookButtonNavigationDiv"));
-        console.log(d.getElementById("FBLogin"));
-        return;
-      }
-      const fjs = d.getElementsByTagName(s)[0];
-      const js = d.createElement(s); 
-      js.id = id;
-      js.src = `https://connect.facebook.net/en_US/sdk.js`;
-      fjs.parentNode.insertBefore(js, fjs);
-      js.onload = () => {
-        ReactDOM.render(<div id="FBLogin" className="fb-login-button" data-size="large" data-button-type="login_with" data-layout="default" data-auto-logout-link="false" data-use-continue-as="false" data-width=""></div>, d.getElementById("facebookButtonNavigationDiv"));
-      };
-    })(document, 'script', 'facebook-jssdk');
-  }, [checkLoginState]);
-
   if (isLoggedIn) {
     return(
       <Redirect to="/"></Redirect>
@@ -223,8 +134,6 @@ export default function Login({onLogin}) {
           >
             Sign In
           </Button>
-
-          <div id="facebookButtonNavigationDiv"></div>
 
           <Grid container justify="flex-end">
             <Grid item>
